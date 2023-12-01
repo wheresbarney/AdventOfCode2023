691cc12a19054522eb3bd67a35d629b57cad7155
Barney Moss <barneym@fb.com>
2023-12-01 11:37:25 +0000

    First day complete 🎄✨

---
+++ aoc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.10
+# https://adventofcode.com/2023
+
+from sys import argv
+from time import perf_counter
+
+# day func input
+day = 'day' + argv[1]
+src = f'./{day}/{day}.py'
+function = argv[2]
+input = f'./{day}/{argv[3]}.txt'
+
+with open(src, 'rb') as f:
+    code = compile(f.read(), src, 'exec')
+exec(code)
+
+with open(input, 'r') as f:
+    data = [l.rstrip() for l in f]
+
+t1 = perf_counter()
+if '1' in function:
+    output = q1(data)
+else:
+    output = q2(data)
+t2 = perf_counter()
+
+print(output)
+print(f"Execution time: {t2 - t1:0.4f} seconds")
---
+++ day1/day1.py
@@ -0,0 +1,37 @@
+# https://adventofcode.com/2023/day/1
+
+def parse(input):
+    return input
+
+
+def q1(lines):
+    sum = 0
+    for line in lines:
+        digits = [c for c in line if c.isdecimal()]
+        sum += int(digits[0] + digits[-1])
+    return sum
+
+
+def q2(lines):
+    sum = 0
+    for line in lines:
+        first = last = None
+        for i in range(len(line)):
+            if (signal := extract_signal(line[i:])):
+                last = signal
+                if not first:
+                    first = last
+        sum += int(str(first) + str(last))
+    return sum
+
+
+WORDS_TO_NUMS = {s: i+1 for i, s in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'])}
+
+def extract_signal(s):
+    if s[0].isdecimal():
+        return int(s[0])
+    for w, n in WORDS_TO_NUMS.items():
+        if s[:len(w)] == w:
+            return n
+
+    return None
