6f79a4e857968e205a7ca702e66ee113e78ee24e
Barney Moss <barneym@fb.com>
2023-12-02 16:11:55 +0000

    Fornatting and linting only

--- day2/day2.py
+++ day2/day2.py
@@ -2,25 +2,29 @@
 
 from math import prod
 
+
 def parse_dice(dice):
-  count, colour = dice.split(' ')
-  return int(count), colour
+    count, colour = dice.split(" ")
+    return int(count), colour
 
 
 def parse(input):
     games = []
     for line in input:
-        glimpses = [glimpse.split(', ') for glimpse in line.split(': ')[1].split('; ')]
-        games.append([[parse_dice(dice) for dice in glimpse] for glimpse in glimpses])
+        glimpses = [glimpse.split(", ") for glimpse in
+                    line.split(": ")[1].split("; ")]
+        games.append([[parse_dice(dice) for dice in
+                     glimpse] for glimpse in glimpses])
     return games
 
 
 def q1(lines):
     games = parse(lines)
     total = 0
-    limits = {'red': 12, 'green': 13, 'blue': 14}
+    limits = {"red": 12, "green": 13, "blue": 14}
     for i, game in enumerate(games):
-        if not [colour for t in game for count, colour in t if count > limits[colour]]:
+        if not [colour for t in game for count, colour in t
+                if count > limits[colour]]:
             total += i + 1
     return total
 
@@ -28,6 +32,11 @@ def q1(lines):
 def q2(lines):
     total = 0
     for game in parse(lines):
-        colours = ({t[1] for a in game for t in a})
-        total += prod([max([t[0] for a in game for t in a if t[1] == colour]) for colour in colours])
+        colours = {t[1] for a in game for t in a}
+        total += prod(
+            [
+                max([t[0] for a in game for t in a if t[1] == colour])
+                for colour in colours
+            ]
+        )
     return total
