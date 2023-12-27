fa3c350f4c21d4868eef45074082687d24fd5845
Barney Moss <barneym@fb.com>
2023-12-27 20:41:35 +0000

    Day 25: loaded the input into graphviz and did it by eye!

--- day25/day25.py
+++ day25/day25.py
@@ -1,1 +1,37 @@
 # https://adventofcode.com/2023/day/25
+
+
+def q1(input):
+    nodes = {
+        src: {*dests.split()} for src, dests in [line.split(": ") for line in input]
+    }
+
+    for src, dests in list(nodes.items()):
+        for dest in dests:
+            if dest not in nodes:
+                nodes[dest] = set()
+            nodes[dest].add(src)
+
+    breaks = (
+        (("hfx", "pzl"), ("bvb", "cmg"), ("nvd", "jqt"))
+        if len(input) == 13
+        else (("nvb", "fts"), ("qmr", "kzx"), ("jff", "zns"))
+    )
+    for n1, n2 in breaks:
+        nodes[n1].remove(n2)
+        nodes[n2].remove(n1)
+
+    return len(all_connected(breaks[0][0], nodes, set())) * len(
+        all_connected(breaks[0][1], nodes, set())
+    )
+
+
+def all_connected(node, nodes, seen):
+    if node in seen:
+        return seen
+    seen.add(node)
+
+    for n in nodes[node]:
+        seen |= all_connected(n, nodes, seen)
+
+    return seen
